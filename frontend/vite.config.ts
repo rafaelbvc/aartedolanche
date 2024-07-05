789c092e826778a262cf7910317ead2ed4b61b68
rafaelbvc <rafaelbvc@hotmail.com>
2024-07-05 16:52:30 -0300

    Docker BugFixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,12 +6,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4050, // definir a porta para 4050
-  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/__tests__/setup.ts'],
   },
 });
+
+// server: {
+//   port: 5090, // definir a porta para 5090
+// },
