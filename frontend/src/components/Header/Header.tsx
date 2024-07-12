154e18416d5b16b3e45beca9626e7cc80a95a58d
rafaelbvc <rafaelbvc@hotmail.com>
2024-07-12 11:02:10 -0300

    v.0.4.7 - Header Sitcky

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import NavBar from "../NavBar/NavBar";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Header.module.css";
+import logoMain from "../../assets/pngs/logoMain.png";
 
 const Header = () => {
   return (
     <header className={styles.headerComponent}>
+      <img src={logoMain} alt="logo png background alpha" className={styles.logoMain} />
       <SearchBar />
-      <div>banner</div>
+
       <NavBar />
     </header>
   );
