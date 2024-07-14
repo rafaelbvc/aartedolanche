ca0e1053f4f7248ee9ba6eb6b81d2ae8e79441c5
rafaelbvc <rafaelbvc@hotmail.com>
2024-07-14 11:51:11 -0300

    v.0.5.3 - Footer Svgs

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,16 +1,60 @@
-import "./Footer.module.css";
+import SvgComponent from "../SvgComponent";
+import styles from "./Footer.module.css";
+import {
+  whatsapp,
+  facebook,
+  instagram,
+  email,
+  mobilePhone,
+  phone,
+  map,
+  arrowup,
+} from "../../assets/svgs/index";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>pequena descricao</p>
-      <hr />
+    <footer className={styles.footerApp}>
+      <section>
+        <p>pequena descricao</p>
+        <hr />
+      </section>
       {/* <p>topbtn absolute </p> */}
-      <p>contatos abrir o app</p>
-      <hr />
-      <p>localizacao pagina google maps</p>
-      <hr />
-      <p>Footer &copy;</p>
+      <section>
+        <p>SIGA-NOS</p>
+        <menu>
+          <SvgComponent
+            svgImage={whatsapp}
+            imgAlt="whatsapp icon"
+            svgWidth=""
+          />
+          <SvgComponent
+            svgImage={facebook}
+            imgAlt="facebook icon"
+            svgWidth=""
+          />
+          <SvgComponent
+            svgImage={instagram}
+            imgAlt="instagram icon"
+            svgWidth=""
+          />
+          <SvgComponent svgImage={email} imgAlt="email icon" svgWidth="" />
+          <SvgComponent
+            svgImage={mobilePhone}
+            imgAlt="mobilePhone icon"
+            svgWidth=""
+          />
+          <SvgComponent svgImage={phone} imgAlt="phone icon" svgWidth="" />
+        </menu>
+        <hr />
+      </section>
+      <section>
+        <p>localizacao pagina google maps</p>
+        <hr />
+      </section>
+      <section>
+        <p>Copyright&copy; 2024 A Arte do Lanche</p>
+        <hr />
+      </section>
       <p>Desenvolvedor RBVC Solucoes</p>
     </footer>
   );
