0f0559efd82fd658f53b449070ab82b23f6f9a33
rafaelbvc <rafaelbvc@hotmail.com>
2024-07-20 12:54:42 -0300

    v.0.6.7 - Frontend CardComponents Filled

---
+++ frontend/src/assets/burgers/index.ts
@@ -0,0 +1,6 @@
+import burgerA from "./burger-a.jpg";
+import burgerB from "./burger-b.jpg";
+import burgerC from "./burger-c.jpg";
+import burgerD from "./burger-d.jpg";
+
+export { burgerA, burgerB, burgerC, burgerD };
--- frontend/src/components/CardsContainer/CardsContainer.tsx
+++ frontend/src/components/CardsContainer/CardsContainer.tsx
@@ -1,34 +1,98 @@
+import CardComponent from "../CardComponent/CardComponent";
 import styles from "./CardsContainer.module.css";
 import { useState, useEffect } from "react";
+import * as photoPath from "../../assets/burgers/index";
 
 const url = "http://localhost:10808/hamburgers";
 
+interface IBurgers {
+  burgerName: string;
+  burgerPhotoPath: string;
+  burgerDescription: string;
+  burgerIngredients: string;
+  burgerPrice: string;
+  burgerLikes: string;
+}
+
+/* <Promise<Array<IBurgers>>></Promise> */
+console.log(photoPath.burgerD);
+
 const CardsContainer = () => {
-  const [burgerData, setBurgerData] = useState<any>(undefined);
+  const [burgerData, setBurgerData] = useState([]);
 
   useEffect(() => {
-    const fetchBurgerData = async () => {
-      try {
-        const data: any = await fetch(url, {
-          headers: { "Content-Type": "application/json" },
-        });
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => setBurgerData(result));
+    // () => setBurgerData(result)
+    // .catch((error) => console.log(error, "deu merda"));
+    // setBurgerData(result);
+    // result.map(
+    //   (
+    //     // burgerLikes,
+    //     // burgerName,
+    //     // burgerPhoto,
+    //     // burgerPhotoAlt,
+    //     // burgerPrice,
+    //     // burgerDescription,
+    //     // burgerIngredients,
+    //     itens,
+    //   ) => {
+    //     arr.push(...itens, itens);
+    //     console.log(arr);
+    //     setBurgerData(arr);
+    //   },
+    // );
+    // setBurgerData(result);
+    // const renderContainer = (
+    //   <CardComponent
+    //     burgerDescription={burgerDescription}
+    //     burgerIngredients={burgerIngredients}
+    //     burgerRate={0}
+    //     burgerPhoto={burgerPhoto}
+    //     burguerPrice={burgerPrice}
+    //     burgerPhotoAlt={""}
+    //     burgerName={burgerName}
+    //   />
+    //);
+    // setBurgerData(renderContainer);
+    // console.log(dataBurger);
 
-        console.log(data.json(), "datajson");
-        const dataBurger = JSON.stringify(data);
-        console.log(dataBurger);
-        setBurgerData(dataBurger);
-      } catch (e) {
-        console.log(e, "error");
-      }
-    };
-    fetchBurgerData();
-  }, []);
+    // setBurgerData(result);
+    // });
+    // const fetchBurgerData = async () => {
+    //   try {
+    //     const data: any = await fetch(url, {
+    //       headers: { "Content-Type": "application/json" },
+    //     });
+    //     const dataB = data.json();
+    //     console.log(dataB, "dataB");
+    //     setBurgerData(dataB);
+    //   } catch (e) {
+    //     console.log(e, "error");
+    //   }
+    // };
+    // console.log(burgerData, "burger data");
+    // fetchBurgerData();
+  }, [setBurgerData]);
 
-  console.log(burgerData, "burger data");
   return (
     <div className={styles.cardsContainer}>
-      {/* <CardComponent burgerPhoto={photo}/>
-      <CardComponent />
+      {/* <CardComponent burgerPhoto={burgerData} /> */}
+      {burgerData &&
+        burgerData.map((data) => (
+          <CardComponent
+            key={data.burgerName}
+            burgerDescription={data.burgerDescription}
+            burgerIngredients={data.burgerIngredients}
+            burgerRate={0}
+            burgerPhoto={data.burgerPhotoPath}
+            burguerPrice={data.burgerPrice}
+            burgerPhotoAlt={""}
+            burgerName={data.burgerName}
+          />
+        ))}
+      {/*<CardComponent />
       <CardComponent />
       <CardComponent />
       <CardComponent />
