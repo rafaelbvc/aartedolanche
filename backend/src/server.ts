5047c37893fcec68922249e72b6e505433252c43
rafaelbvc <rafaelbvc@hotmail.com>
2024-07-19 22:52:07 -0300

    v.0.6.2 - Backend new config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,9 @@
 import express from "express"
-import { config } from "dotenv"
-import { MongoClient } from "./db/mongo"
 
-const Server = async() => {
-    config()
+
+
 
 const server = express()
 
-server.use(express.json())
 
-await MongoClient.connect()
-
-const port = process.env.PORT || 8002
-
-server.listen(port, () => console.log(`listening on port ${port}...`))
-
-}
-
-Server()
+server.listen(10600, () => console.log(`servidor up porta 10600`))
